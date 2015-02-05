110653ab93a1d5aa601a7758c5bd22679faf8daf
GP89Developers <perezgabriel89@gmail.com>
2015-02-05 19:22:45 -0400

    refectory ForecastService

--- app/src/main/java/com/example/android/sunshine/app/ForecastService.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastService.java
@@ -37,9 +37,11 @@ public class ForecastService {
             final String QUERY_PARAM = "q";
             final String UNITS_PARAM = "units";
             final String DAYS_PARAM = "cnt";
+            final String FORMAT_PARAM = "mode";
 
             Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
                     .appendQueryParameter(QUERY_PARAM, params)
+                    .appendQueryParameter(FORMAT_PARAM,format)
                     .appendQueryParameter(UNITS_PARAM, units)
                     .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
                     .build();
